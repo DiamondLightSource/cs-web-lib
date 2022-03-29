f063b004cdf62158c6dfa1333cee1c8d67c56ddf
Rebecca Williams <rjw@observatorysciences.co.uk>
2022-03-29 13:03:04 +0100

    Fixing css to display line breaks in label text.

--- src/ui/widgets/Label/label.tsx
+++ src/ui/widgets/Label/label.tsx
@@ -86,7 +86,7 @@ export const LabelComponent = (
 
   if (wrapWords) {
     style["wordBreak"] = "break-word";
-    style["whiteSpace"] = "normal";
+    style["whiteSpace"] = "pre-line";
   }
 
   // Simple component to display text - defaults to black text and dark grey background
