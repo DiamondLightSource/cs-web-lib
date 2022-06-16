59587a80f35d2a75cc551c129db695b5b9571427
Rebecca Williams <rjw@observatorysciences.co.uk>
2022-06-16 09:43:20 +0100

    Add comment to describe addition of timestamp
    This describes why the timestamp has to be appended to the image src
    so that it can be refreshed.

--- src/ui/widgets/Image/image.tsx
+++ src/ui/widgets/Image/image.tsx
@@ -57,6 +57,11 @@ export const ImageComponent = (
 
   // Should we be refreshing image instead of using cache
   let imageFileName = props.imageFile;
+  // If a filename has been appended with '#' or '#{number}' this indicates
+  // that it should be refreshed. The only way to refresh the image from it's
+  // source instead of the browser cache is to further append the timestamp
+  // to the filename. As it follows the '#', it will not affect the file
+  // look-up location.
   if (imageFileName.includes("#")) {
     imageFileName = imageFileName + new Date().getTime();
   }
