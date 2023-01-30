891db069a8c98d437202daf6ebb9f1f4bec4687a
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2023-01-30 15:50:36 +0000

    Create webcam widget

---
+++ src/ui/widgets/Webcam/webcam.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { contextRender } from "../../../testResources";
+import { fireEvent } from "@testing-library/react";
+import { Webcam } from "./webcam";
+
+describe("<Webcam />", (): void => {
+  test("it displays the alt text while loading", (): void => {
+    const { getByAltText } = contextRender(
+      <Webcam url="http://fake-stream.diamond.ac.uk/video.mjpg" />
+    );
+    expect(getByAltText("Loading Webcam MJPEG stream...")).toBeInTheDocument();
+  });
+
+  test("it displays the alt text once loaded", (): void => {
+    const { getByAltText } = contextRender(
+      <Webcam url="http://fake-stream.diamond.ac.uk/video.mjpg" />
+    );
+    fireEvent.load(
+      getByAltText("Loading Webcam MJPEG stream...") as HTMLImageElement
+    );
+    expect(
+      getByAltText(
+        "Webcam MJPEG stream at http://fake-stream.diamond.ac.uk/video.mjpg"
+      )
+    ).toBeInTheDocument();
+  });
+
+  test("it displays the alt text when error occurs", (): void => {
+    const { getByAltText } = contextRender(
+      <Webcam url="http://fake-stream.diamond.ac.uk/video.mjpg" />
+    );
+    fireEvent.error(
+      getByAltText("Loading Webcam MJPEG stream...") as HTMLImageElement
+    );
+    expect(
+      getByAltText(
+        "Connection could not be made to Webcam MJPEG stream at http://fake-stream.diamond.ac.uk/video.mjpg"
+      )
+    ).toBeInTheDocument();
+  });
+});
---
+++ src/ui/widgets/Webcam/webcam.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import classes from "./webcam.module.css";
+
+/**
+ * Creates a Webcam component for displaying MJPEG
+ * streams
+ * @param props url of webcam stream
+ * @returns HTML component
+ */
+export const Webcam = (props: { url: string }): JSX.Element => {
+  const { width, height } = useWindowDimensions();
+  // Create image tag
+  const webcamImg = (
+    <div
+      style={{
+        height: height,
+        width: width,
+        fontSize: "14px", // Set properties for image alt text
+        fontWeight: "bold",
+        color: "#a6190f"
+      }}
+    >
+      <img
+        id="stream"
+        className={classes.img}
+        src={props.url}
+        alt={`Loading Webcam MJPEG stream...`}
+        onError={onError}
+        onLoad={onLoad}
+      />
+    </div>
+  );
+  return webcamImg;
+};
+
+/**
+ * Returns an error visible to the user when the MJPEG stream
+ * at the given URL cannot be reached.
+ */
+function onError() {
+  const image = document.getElementById("stream") as HTMLImageElement;
+  const alt = `Connection could not be made to Webcam MJPEG stream at ${image.src}`;
+  (document.getElementById("stream") as HTMLImageElement).alt = alt;
+  console.error(`Connection to ${image.src} failed`);
+}
+
+/**
+ * Changes image alt text once image is loaded.
+ */
+function onLoad() {
+  const image = document.getElementById("stream") as HTMLImageElement;
+  const alt = `Webcam MJPEG stream at ${image.src}`;
+  (document.getElementById("stream") as HTMLImageElement).alt = alt;
+}
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
