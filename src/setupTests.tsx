349448886e36b5a0ea061abed5e8ff0400f6fc5d
Rebecca Williams <rjw@observatorysciences.co.uk>
2023-03-03 13:19:57 +0000

    Fix formatting

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -5,7 +5,7 @@ import log from "loglevel";
 log.setLevel("info");
 
 // Required to stop console errors about missing canvas
-require('jest-canvas-mock');
+require("jest-canvas-mock");
 
 // Plotly expects this function to exist but it doesn't
 // when testing.
