53dc1a9d9468417a830bb36faa9685d4422df55f
Rebecca Williams <rjw@observatorysciences.co.uk>
2023-11-27 17:17:59 +0000

    Add scripts to open multiple Firefox windows

---
+++ performance/open_windows/open_ff_windows.js
@@ -0,0 +1,10 @@
+nPages = 50;
+
+for (var i = 0; i < nPages; i++) {
+  if (i > 0) {
+    window.open("http://localhost:3000/performancePage"+i, "_blank")
+  }
+  else {
+    window.open("http://localhost:3000/performancePage", "_blank")
+  }
+}
