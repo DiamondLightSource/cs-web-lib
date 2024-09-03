5fae0c45520276897ee10466087a653cbd619534
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-09-03 15:56:57 +0100

    Update eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
-        plugins: [react()],
+        plugins: [react(), eslint()],
         test: {
             environment: 'jsdom',
             include: ['**/*.test.ts', '**/*.test.tsx'],
