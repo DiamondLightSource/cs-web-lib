d94889c44b86c9bc925d9b8816816148811a2a96
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2024-09-03 10:43:56 +0100

    Add missing letter to make tests run for widgets too

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
         plugins: [react()],
         test: {
             environment: 'jsdom',
-            include: ['**/*.test.ts', '**/*.test.ts'],
+            include: ['**/*.test.ts', '**/*.test.tsx'],
             globals: true,
             setupFiles: 'src/setupTests.tsx'
         },
