ac08580dc4dab5cd3c1f4b7f9b3d180ddae4d9fc
David Perl <david.perl@diamond.ac.uk>
2024-11-15 11:48:16 +0000

    fix rollup build and add to CI

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,38 +4,42 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 import postcss from "rollup-plugin-postcss";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-const packageJson = require("./package.json");
-
-export default [
+const config = [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        file: "dist/cjs/index.js",
         format: "cjs",
-        sourcemap: true,
+        sourcemap: true
       },
       {
-        file: packageJson.module,
+        file: "dist/esm/index.js",
         format: "esm",
-        sourcemap: true,
-      },
+        sourcemap: true
+      }
     ],
     plugins: [
       peerDepsExternal(),
       resolve({ preferBuiltins: true }),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        compilerOptions: { outDir: "dist" }
+      }),
       postcss(),
-    ],
+      dts()
+    ]
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
 
-    external: [/\.css$/],
-  },
+    external: [/\.css$/]
+  }
 ];
+
+export default config;
