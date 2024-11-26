d76ed187ee083a796a151e6bfbafc2fcda04ac82
David Perl <david.perl@diamond.ac.uk>
2024-11-26 16:51:43 +0000

    actually add the index files lol

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./redux";
 export * from "./misc";
+export * from "./connection";
 export * from "./ui/components";
 export * from "./ui/widgets";
+export * from "./ui/hooks";
 export * from "./types";
 export { contextRender } from "./testResources";
