c93d0706f1915eae5a58a5038eec15da838894db
NatLeung96 <nathanael.leung96@gmail.com>
2025-03-21 09:09:39 +0000

    Reformatted the script

--- src/colourscheme.ts
+++ src/colourscheme.ts
@@ -1,12 +1,12 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 export const defaultColours = createTheme({
-    palette: {
-        primary: {
-            main: "#C0C0C0",
-            // light: currently calculated automatically by MUI
-            // dark: currently calculated automatically by MUI
-            //contrastText: currently calculated automatically by MUI
-        },
+  palette: {
+    primary: {
+      main: "#C0C0C0",
+      // light: currently calculated automatically by MUI
+      // dark: currently calculated automatically by MUI
+      // contrastText: currently calculated automatically by MUI
     },
-})
+  },
+});
