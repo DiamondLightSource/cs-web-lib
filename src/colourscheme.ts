369923a64ec495c21ab800ebb95a30ad2dd3b6c4
NatLeung96 <nathanael.leung96@gmail.com>
2025-03-21 09:11:14 +0000

    Reformatted again

--- src/colourscheme.ts
+++ src/colourscheme.ts
@@ -3,10 +3,10 @@ import { createTheme } from "@mui/material/styles";
 export const defaultColours = createTheme({
   palette: {
     primary: {
-      main: "#C0C0C0",
+      main: "#C0C0C0"
       // light: currently calculated automatically by MUI
       // dark: currently calculated automatically by MUI
       // contrastText: currently calculated automatically by MUI
-    },
-  },
+    }
+  }
 });
