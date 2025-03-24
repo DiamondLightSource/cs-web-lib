f9f3db90db03c8d0636d1b5482cf8a34388dd7a0
NatLeung96 <nathanael.leung96@gmail.com>
2025-03-24 09:12:24 +0000

    Changed default colours

--- src/colourscheme.ts
+++ src/colourscheme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material/styles";
 export const defaultColours = createTheme({
   palette: {
     primary: {
-      main: "#000080",
+      main: "#C0C0C0",
       // light: currently calculated automatically by MUI
       // dark: currently calculated automatically by MUI
       contrastText: "#FFFFFF"
