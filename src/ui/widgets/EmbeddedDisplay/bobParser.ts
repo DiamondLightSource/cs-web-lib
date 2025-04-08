e95b6a5b8d43a2a669a65735a3178a775357e77e
NatLeung96 <nathanael.leung96@gmail.com>
2025-04-08 08:55:46 +0100

    Added width and height to parser

--- src/ui/widgets/EmbeddedDisplay/bobParser.ts
+++ src/ui/widgets/EmbeddedDisplay/bobParser.ts
@@ -380,7 +380,9 @@ export function parseBob(
     rotation: ["rotation", bobParseNumber],
     styleOpt: ["style", bobParseNumber],
     lineColor: ["line_color", opiParseColor],
-    rotationStep: ["rotation_step", bobParseNumber]
+    rotationStep: ["rotation_step", bobParseNumber],
+    width: ["width", bobParseNumber],
+    height: ["height", bobParseNumber]
   };
 
   const complexParsers = {
