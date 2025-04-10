97dc4c91c67c8b625864ada2a887eef40cda6a22
NatLeung96 <84187731+NatLeung96@users.noreply.github.com>
2025-04-10 16:08:37 +0100

    87 replace choice button with mui button group (#98)
    * Replaced base react button group with MUI ToggleButtonGroup
    
    * Commented out expect statements checking the styling
    
    * Removed ThemeProvider
    
    * Moved static styling to styled API
    
    * Updated tests
    
    * Removed redundant height and width for ButtonGroup
    
    * Set opacity to 0.6 for hover and selected-hover
    
    * Added borders to buttons and fixed text wrapping
    
    * Updated snapshots
    
    * Deleted redundant CSS file
    
    * Removed CSS import
    
    * Changed button height/width and hover opacity
    
    * Updated snapshots

--- src/ui/widgets/ChoiceButton/choiceButton.test.tsx
+++ src/ui/widgets/ChoiceButton/choiceButton.test.tsx
@@ -13,15 +13,14 @@ describe("<BoolButton />", (): void => {
     const choiceButtonProps = {
       value: null
     };
-    const { getAllByRole } = render(ChoiceButtonRenderer(choiceButtonProps));
+    const { getAllByRole, asFragment } = render(
+      ChoiceButtonRenderer(choiceButtonProps)
+    );
     const buttons = getAllByRole("button") as Array<HTMLButtonElement>;
 
     expect(buttons[0].textContent).toEqual("Item 1");
     expect(buttons[1].textContent).toEqual("Item 2");
-    expect(buttons[0].style.height).toEqual("43px");
-    expect(buttons[1].style.width).toEqual("46px");
-    expect(buttons[0].style.backgroundColor).toEqual("rgb(210, 210, 210)");
-    expect(buttons[1].style.fontSize).toEqual("0.875rem");
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test("pass props to widget", (): void => {
@@ -36,19 +35,15 @@ describe("<BoolButton />", (): void => {
       itemsFromPv: false,
       enabled: false
     };
-    const { getAllByRole } = render(ChoiceButtonRenderer(choiceButtonProps));
+    const { getAllByRole, asFragment } = render(
+      ChoiceButtonRenderer(choiceButtonProps)
+    );
     const buttons = getAllByRole("button") as Array<HTMLButtonElement>;
 
     expect(buttons.length).toEqual(4);
-    // First button is selected therefore different color and box shadow
-    expect(buttons[0].style.boxShadow).toEqual(
-      "inset 0px 23px 35px 0px rgba(0,0,0,0.3)"
-    );
-    expect(buttons[0].style.backgroundColor).toEqual("rgb(10, 60, 40)");
     expect(buttons[2].textContent).toEqual("Setting");
-    expect(buttons[3].style.cursor).toEqual("not-allowed");
-    expect(buttons[3].style.height).toEqual("31px");
-    expect(buttons[3].style.backgroundColor).toEqual("rgb(20, 20, 200)");
+    expect(buttons[3]).toHaveProperty("disabled", true);
+    expect(asFragment()).toMatchSnapshot();
   });
 
   test("pass props to widget, using itemsFromPv", (): void => {
