6d9f05c1391905a728f1b7b1e94349042992ec76
NatLeung96 <nathanael.leung96@gmail.com>
2025-05-27 09:29:07 +0100

    Created file for tank widget with a scale

---
+++ src/ui/widgets/Tank/tankWithScale.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Color, Font } from "../../../types";
+import classes from "./tank.module.css";
+
+export const TankWithScale = (props: {
+  min: number;
+  max: number;
+  emptyColor: Color;
+  fillColor: Color;
+  foregroundColor: Color;
+  backgroundColor: Color;
+  font: Font | undefined;
+  percent: number;
+  width: number;
+  outline: string;
+}): JSX.Element => {
+  const {
+    min,
+    max,
+    emptyColor,
+    fillColor,
+    foregroundColor,
+    backgroundColor,
+    font,
+    percent,
+    width,
+    outline
+  } = props;
+  return (
+    <span
+      className={classes.TankBackground}
+      style={{
+        backgroundColor: backgroundColor.toString(),
+        outline: outline
+      }}
+    >
+      <span
+        className={classes.ScaleMarker}
+        style={{
+          height: "30px",
+          width: "100px",
+          top: "10%",
+          color: foregroundColor.toString(),
+          transform: "rotate(-90deg) translateX(15px) translateY(-30px)",
+          ...font?.css()
+        }}
+      >
+        {Number(max).toFixed(1)}
+      </span>
+      <span
+        className={classes.ScaleMarker}
+        style={{
+          height: "30px",
+          width: "100px",
+          top: "50%",
+          color: foregroundColor.toString(),
+          transform: "rotate(-90deg) translateX(15px) translateY(-30px)",
+          ...font?.css()
+        }}
+      >
+        {Number(max / 2).toFixed(1)}
+      </span>
+      <span
+        className={classes.ScaleMarker}
+        style={{
+          height: "30px",
+          width: "100px",
+          top: "90%",
+          color: foregroundColor.toString(),
+          transform: "rotate(-90deg) translateX(15px) translateY(-30px)",
+          ...font?.css()
+        }}
+      >
+        {Number(min).toFixed(1)}
+      </span>
+      <span
+        className={classes.EmptyTank}
+        style={{
+          height: "80%",
+          width: `${width - 30}px`,
+          top: "10%",
+          right: "0%",
+          backgroundColor: emptyColor.toString()
+        }}
+      >
+        <span
+          className={classes.TankFill}
+          style={{
+            backgroundColor: fillColor.toString(),
+            top: `${100 - percent}%`
+          }}
+        ></span>
+      </span>
+    </span>
+  );
+};
