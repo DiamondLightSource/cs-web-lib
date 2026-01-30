dcdebd318848df8edb5caa0f304f366e3951d58b
Abigail Alexander <abigail.alexander@diamond.ac.uk>
2026-01-30 13:06:46 +0000

    Update tab widgets to MUI, support navigation tabs and base tabs

---
+++ src/ui/widgets/Tabs/navigationTabs.tsx
@@ -0,0 +1,84 @@
+/**
+ * A widget that displays widgets in a number of pre-defined
+ * tabs. This displays existing files via Embedded Displays
+ * 
+ * See also the dynamic tabs widget.
+ */
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+
+import { Widget } from "../widget";
+import { WidgetPropType } from "../widgetProps";
+import { registerWidget } from "../register";
+import {
+  InferWidgetProps,
+  ColorPropOpt,
+  ChoicePropOpt,
+  BoolPropOpt,
+  IntPropOpt,
+  FontPropOpt
+} from "../propTypes";
+
+import { TabBar } from "./tabs";
+import { Color, RelativePosition } from "../../../types";
+import { EmbeddedDisplay } from "../EmbeddedDisplay/embeddedDisplay";
+
+export const NavigationTabsProps = {
+  tabs: PropTypes.array.isRequired,
+  direction: IntPropOpt,
+  visible: BoolPropOpt,
+  tabWidth: IntPropOpt,
+  tabHeight: IntPropOpt,
+  tabSpacing: IntPropOpt,
+  selectedColor: ColorPropOpt,
+  deselectedColor: ColorPropOpt,
+  font: FontPropOpt,
+  activeTab: IntPropOpt
+};
+
+export const NavigationTabsComponent = (
+  props: InferWidgetProps<typeof NavigationTabsProps>
+): JSX.Element => {
+
+  const tabChildren = useMemo(() => {
+    return props.tabs.map((tab) => {
+       return {
+        name: tab.name,
+        children: (<EmbeddedDisplay
+          height={"100%"}
+          width={"100%"}
+          position={new RelativePosition()}
+          scroll={true}
+          resize={0}
+          file={{
+            path: tab.file,
+            macros: { ...tab.macros },
+            defaultProtocol: tab.protocol ?? "ca"
+          }}
+      />)}
+    });
+  }, [props.tabs]);
+
+  return (
+    <TabBar
+      direction={1}
+      tabWidth={100}
+      tabSpacing={2}
+      selectedColor={Color.fromRgba(236, 236, 236)}
+      deselectedColor={Color.fromRgba(200, 200, 200)}
+      {...props}
+      tabs={tabChildren}
+    ></TabBar>
+  );
+};
+
+export const NavigationTabsWidgetProps = {
+  ...NavigationTabsProps,
+  ...WidgetPropType
+};
+
+export const NavigationTabs = (
+  props: InferWidgetProps<typeof NavigationTabsWidgetProps>
+): JSX.Element => <Widget baseWidget={NavigationTabsComponent} {...props} />;
+
+registerWidget(NavigationTabs, NavigationTabsProps, "navigationtabs");
--- src/ui/widgets/Tabs/tabs.tsx
+++ src/ui/widgets/Tabs/tabs.tsx
@@ -1,58 +1,139 @@
-import React from "react";
-import classes from "./tabs.module.css";
+import React, { useState } from "react";
+import { Box, Tab as MuiTab, styled, Tabs, useTheme } from "@mui/material";
+import { Color, Font, RelativePosition } from "../../../types";
+import PropTypes from "prop-types";
+import {
+  ColorPropOpt,
+  BoolPropOpt,
+  IntPropOpt,
+  FontPropOpt,
+  FuncPropOpt,
+  InferWidgetProps
+} from "../propTypes";
 
-export interface TabBarProps {
-  tabNames: string[];
-  selectedTab?: number;
-  onTabSelected: (index: number) => void;
-  // If onTabClosed is not provided then no close button
-  // will be added to the tabs.
-  onTabClosed?: (index: number) => void;
-}
+const Tab = styled(MuiTab)({
+  padding: 0,
+  lineHeight: 1.3,
+  "&.MuiTab-root": {
+    padding: 0
+  },
+  "&:hover": {
+    opacity: 0.5
+  }
+});
 
-export const TabBar = (props: TabBarProps): JSX.Element => {
+export const TabBarProps = {
+  tabs: PropTypes.array.isRequired,
+  onTabSelected: FuncPropOpt,
+  onTabClosed: FuncPropOpt,
+  direction: IntPropOpt,
+  tabWidth: IntPropOpt,
+  tabHeight: IntPropOpt,
+  tabSpacing: IntPropOpt,
+  selectedColor: ColorPropOpt,
+  deselectedColor: ColorPropOpt,
+  font: FontPropOpt,
+  activeTab: IntPropOpt,
+  visible: BoolPropOpt
+};
+
+export const TabBar = (
+  props: InferWidgetProps<typeof TabBarProps>
+): JSX.Element => {
+  const theme = useTheme();
+  const {
+    direction = 0,
+    tabWidth = 100,
+    tabHeight = 30,
+    tabSpacing = 0,
+    selectedColor = Color.fromRgba(236, 236, 236),
+    deselectedColor = Color.fromRgba(200, 200, 200),
+    activeTab = 0,
+    visible = true
+  } = props;
+  const [value, setValue] = useState(activeTab);
+  console.log(props.tabs);
+
+  const font =
+    props.font ??
+    new Font(theme.typography.fontSize, undefined, theme.typography.fontFamily);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const element = event.target as HTMLElement;
+    if (element.tagName === "path") {
+      event.preventDefault();
+      return;
+    }
+    setValue(newValue);
+  };
+
   return (
-    <div className={classes.TabBar}>
-      {props.tabNames.map(
-        (tabName, index): JSX.Element => (
+    <Box
+      sx={{
+        display: "flex",
+        visibility: visible ? "visible" : "hidden",
+        flexDirection: direction ? "row" : "column",
+        width: "100%",
+        height: "100%",
+        flexGrow: 1
+      }}
+    >
+      <Tabs
+        sx={{
+          width: direction ? tabWidth : "100%",
+          height: direction ? "100%" : tabHeight,
+          display: "flex",
+          font: font.css(),
+          "& .MuiTabs-indicator": {
+            backgroundColor: "transparent"
+          }
+        }}
+        value={value}
+        onChange={handleChange}
+        orientation={direction ? "vertical" : "horizontal"}
+      >
+        {props.tabs.map(
+          (tab, index): JSX.Element => (
+            <Tab
+              key={index}
+              label={tab.name}
+              sx={{
+                color: "black",
+                "&.Mui-selected": {
+                  backgroundColor: selectedColor.toString(),
+                  color: "black"
+                },
+                backgroundColor: deselectedColor.toString(),
+                marginRight: direction ? "0px" : `${tabSpacing}px`,
+                marginBottom: direction ? `${tabSpacing}px` : "0px",
+                "&.MuiTab-root": {
+                  width: tabWidth,
+                  height: tabHeight,
+                  minWidth: tabWidth,
+                  minHeight: tabHeight
+                }
+              }}
+            ></Tab>
+          )
+        )}
+      </Tabs>
+      {props.tabs.map(
+        (tab, index): JSX.Element => (
           <div
-            key={index}
-            title={tabName}
-            onClick={(): void => {
-              props.onTabSelected(index);
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            key={`${tab.name}${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            style={{
+              height: direction ? "100%" : `calc(100% - ${tabHeight}px)`,
+              overflowY: "scroll",
+              width: direction ? `calc(100% - ${tabWidth}px)` : "100%"
             }}
-            // Close tab on middle click.
-            onMouseDown={(e: React.MouseEvent): void => {
-              if (e.button === 1) {
-                props.onTabClosed?.(index);
-              }
-            }}
-            className={
-              index === props.selectedTab
-                ? `${classes.Tab} ${classes.CloseableTab} ${classes.TabSelected}`
-                : `${classes.Tab} ${classes.CloseableTab}`
-            }
           >
-            <span className={classes.CloseableTabText}>{tabName}</span>
-            {props.onTabClosed && (
-              <button
-                className={classes.TabCloseButton}
-                title="Close tab"
-                onClick={(event): void => {
-                  props.onTabClosed?.(index);
-                  event.stopPropagation();
-                }}
-              >
-                <img
-                  style={{ height: "15px", display: "block" }}
-                  src="/img/x.png"
-                  alt="Close tab"
-                ></img>
-              </button>
-            )}
+            {value === index && <>{tab.children}</>}
           </div>
         )
       )}
-    </div>
+    </Box>
   );
 };
