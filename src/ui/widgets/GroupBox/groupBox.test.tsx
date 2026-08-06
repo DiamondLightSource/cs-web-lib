a3e45304f744645bb395eefbb8671da1eca8cc52
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2026-08-06 11:48:46 +0100

    Wrapped test in provider context

--- src/ui/widgets/GroupBox/groupBox.test.tsx
+++ src/ui/widgets/GroupBox/groupBox.test.tsx
@@ -2,33 +2,49 @@ import React from "react";
 import { GroupBoxComponent } from "./groupBox";
 import { ColorUtils } from "../../../types/color";
 import { render } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const testStore = configureStore({
+  reducer: {
+    style: (state = { classes: {}, currentClass: "DEFAULT" }) => state
+  }
+});
 
 describe("<GroupBoxComponent /> snapshots", (): void => {
   test("it matches the snapshot for Group Box style", (): void => {
     const { asFragment } = render(
-      <GroupBoxComponent
-        name={"Test"}
-        backgroundColor={ColorUtils.WHITE}
-        styleOpt={0}
-      />
+      <Provider store={testStore}>
+        <GroupBoxComponent
+          name={"Test"}
+          backgroundColor={ColorUtils.WHITE}
+          styleOpt={0}
+        />
+      </Provider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
   test("it matches the snapshot for Title Bar style", (): void => {
     const { asFragment } = render(
-      <GroupBoxComponent name={"Title"} styleOpt={1} />
+      <Provider store={testStore}>
+        <GroupBoxComponent name={"Title"} styleOpt={1} />
+      </Provider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
   test("it matches the snapshot for Line style", (): void => {
     const { asFragment } = render(
-      <GroupBoxComponent name={"No Title"} styleOpt={2} />
+      <Provider store={testStore}>
+        <GroupBoxComponent name={"No Title"} styleOpt={2} />
+      </Provider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
   test("it matches the snapshot for no style", (): void => {
     const { asFragment } = render(
-      <GroupBoxComponent name={"None"} styleOpt={3} />
+      <Provider store={testStore}>
+        <GroupBoxComponent name={"None"} styleOpt={3} />
+      </Provider>
     );
     expect(asFragment()).toMatchSnapshot();
   });
@@ -36,7 +52,11 @@ describe("<GroupBoxComponent /> snapshots", (): void => {
 
 describe("<GroupBoxComponent />", (): void => {
   test("it renders the title", (): void => {
-    const grouping = <GroupBoxComponent name={"Test"} styleOpt={1} />;
+    const grouping = (
+      <Provider store={testStore}>
+        <GroupBoxComponent name={"Test"} styleOpt={1} />
+      </Provider>
+    );
     const { getByText } = render(grouping);
     expect(getByText("Test")).toBeInTheDocument();
   });
@@ -44,9 +64,11 @@ describe("<GroupBoxComponent />", (): void => {
   test("it renders child div with text", (): void => {
     const childText = "Testing Child Component";
     const groupingWithChild = (
-      <GroupBoxComponent name={"Test"}>
-        <div>{childText}</div>
-      </GroupBoxComponent>
+      <Provider store={testStore}>
+        <GroupBoxComponent name={"Test"}>
+          <div>{childText}</div>
+        </GroupBoxComponent>
+      </Provider>
     );
     const { getByText } = render(groupingWithChild);
     expect(getByText("Test")).toBeInTheDocument();
