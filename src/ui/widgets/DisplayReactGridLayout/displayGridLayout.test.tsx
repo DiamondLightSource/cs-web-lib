74d01c87462b4c7ced3421e4b8277ddf4621124d
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2026-08-07 11:02:29 +0100

    linting fix

--- src/ui/widgets/DisplayReactGridLayout/displayGridLayout.test.tsx
+++ src/ui/widgets/DisplayReactGridLayout/displayGridLayout.test.tsx
@@ -1,11 +1,10 @@
 import { describe, it, expect, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import React from "react";
 
 import { DisplayGridLayoutComponent } from "./displayGridLayout";
 import { MacroContext } from "../../../types/macros";
 import { createMockStyle } from "../../../test-utils/styleTestUtils";
-import { fireEvent } from "@testing-library/react";
 
 vi.mock("react-grid-layout", async () => {
   const actual = await vi.importActual<any>("react-grid-layout");
