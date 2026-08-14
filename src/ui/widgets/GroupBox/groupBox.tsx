811a76146d2ab6b1dca655905da851eb2612892a
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2026-08-14 10:39:50 +0100

    Linting fix

--- src/ui/widgets/GroupBox/groupBox.tsx
+++ src/ui/widgets/GroupBox/groupBox.tsx
@@ -1,7 +1,7 @@
 import React, { CSSProperties, useContext } from "react";
 
 import { Widget } from "../widget";
-import { WidgetPropType } from "../widgetProps";
+import { WidgetPropType , ComponentProps } from "../widgetProps";
 import { registerWidget } from "../register";
 import {
   ChildrenPropOpt,
@@ -14,7 +14,6 @@ import {
   MacrosPropOpt
 } from "../propTypes";
 import { fontToCss, newFont } from "../../../types/font";
-import { ComponentProps } from "../widgetProps";
 import { ColorUtils } from "../../../types/color";
 import Box from "@mui/material/Box";
 import { MacroContext, MacroContextType } from "../../../types/macros";
